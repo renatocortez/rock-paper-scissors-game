af3d5ba7f5356733001a043bc5da323023747956
renatocortez <recortez@gmail.com>
2022-04-07 17:40:39 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,14 +8,48 @@ const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
-rock_div.addEventListener('click', function () {
-    game("r");  
-});
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
 
-paper_div.addEventListener('click', function () {
-    game("p");  
-});
+function win() {
+    
+}
 
-scissors_div.addEventListener('click', function () {
-    game("s");  
-});
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win();
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      win();
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      win();
+      break;
+  }
+}
+
+function main() {
+  rock_div.addEventListener("click", function () {
+    game("r");
+  });
+
+  paper_div.addEventListener("click", function () {
+    game("p");
+  });
+
+  scissors_div.addEventListener("click", function () {
+    game("s");
+  });
+}
+main();
